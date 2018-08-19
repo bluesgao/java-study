3e704e8d254e02ca8ba889c3261a7e3de75a021a
bluesgao <bluesgao@126.com>
2018-08-19 23:57:37 +0800

    ssm

---
+++ ssm-demo/src/main/java/com/bluesgao/ssm/demo/domain/User.java
@@ -0,0 +1,65 @@
+package com.bluesgao.ssm.demo.domain;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String userName;
+
+    private String nickName;
+
+    private Byte userAge;
+
+    private Boolean userSex;
+
+    private Date modifyTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public Byte getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Byte userAge) {
+        this.userAge = userAge;
+    }
+
+    public Boolean getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(Boolean userSex) {
+        this.userSex = userSex;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
