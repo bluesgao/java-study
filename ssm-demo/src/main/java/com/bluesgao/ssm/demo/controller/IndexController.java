da3f41ab02a4ad4d6513da6f462f1abb08869981
bluesgao <bluesgao@126.com>
2018-08-19 23:51:22 +0800

    ssm

--- ssm-demo/src/main/java/com/bluesgao/ssm/demo/controller/IndexController.java
+++ ssm-demo/src/main/java/com/bluesgao/ssm/demo/controller/IndexController.java
@@ -1,4 +1,16 @@
 package com.bluesgao.ssm.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
 public class IndexController {
+    @RequestMapping("/")
+    public String hello(Model model){
+        model.addAttribute("name", "gx");
+        return "index";
+    }
 }
